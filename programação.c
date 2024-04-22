2844c5bb6d8cee85e5ad9635a38151fcca8b1060
miguel <miguwl.silva236@etec.sp.gov.br>
2024-04-22 17:32:14 -0300

    miguel  lindo

---
+++ programação.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+void main(){
+    int nu1;
+    int nu2;
+    int resultado;
+
+printf("digite um valor \n");
+scanf("%d", &nu1 );
+	printf("digite outro valor \n");
+	scanf("%d", &nu2);
+ resultado = nu1-nu2;
+
+
+printf("o resultado da subtração dos numeros %d e %d é: %d \n", nu1,nu2,resultado);
+
+
+
+
+}	
